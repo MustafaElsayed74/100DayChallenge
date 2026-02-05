4cba9658c98781f69ae36c56c77a8d39e12b1011
0x0desha74 <mudesha74@gmail.com>
2026-02-05 18:59:08 +0200

    fixes01

--- ClientApp/capacitor.config.ts
+++ ClientApp/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.habitat',
   appName: '100DayHabitat',
-  webDir: 'dist/ClientApp'
+  webDir: 'dist/ClientApp/browser'
 };
 
 export default config;
